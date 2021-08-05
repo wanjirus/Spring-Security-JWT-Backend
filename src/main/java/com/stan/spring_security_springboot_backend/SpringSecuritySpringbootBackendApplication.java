cb3536881bf2b2951f001e4f417f0d7d2709f647
USER <stanmburu254@gmail.com>
2021-08-05 13:38:51 +0300

    spring security

---
+++ src/main/java/com/stan/spring_security_springboot_backend/SpringSecuritySpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stan.spring_security_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySpringbootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stan/spring_security_springboot_backend/SpringSecuritySpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.spring_security_springboot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySpringbootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
